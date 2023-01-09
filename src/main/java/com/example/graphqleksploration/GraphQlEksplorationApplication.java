211934396221762079d79880042bf573e2d4e987
MUHAMMAD IRHAM LUTHFI <muhammad.irham01@ui.ac.id>
2023-01-09 13:37:54 +0700

    Initiate project

---
+++ src/main/java/com/example/graphqleksploration/GraphQlEksplorationApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqleksploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlEksplorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlEksplorationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphqleksploration/GraphQlEksplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqleksploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlEksplorationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
